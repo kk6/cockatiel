e06a17f3126a8bb1f45d285eeb9b8f2852a33369
kk6 <hiro.ashiya@gmail.com>
2022-03-26 18:55:34 +0900

    :sparkles: Add markdown editor

---
+++ src/components/editor.tsx
@@ -0,0 +1,19 @@
+import '@uiw/react-md-editor/markdown-editor.css'
+import '@uiw/react-markdown-preview/markdown.css'
+import dynamic from 'next/dynamic'
+import { useState } from 'react'
+
+const MDEditor = dynamic(
+  // @ts-ignore
+  () => import('@uiw/react-md-editor').then((mod) => mod.default),
+  { ssr: false }
+)
+
+export const Editor: React.VFC = () => {
+  const [value, setValue] = useState('**Hello world!!!**')
+  return (
+    <div data-color-mode="light">
+      <MDEditor value={value} onChange={setValue} />
+    </div>
+  )
+}
